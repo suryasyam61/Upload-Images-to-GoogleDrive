9c3b56f0c7d5da5dd27924aefcb181d8f1d3854b
Syam <suryasyam61@gmail.com>
2022-11-22 16:19:43 +0530

    s

--- App.js
+++ App.js
@@ -204,7 +204,7 @@ app.post('/upload', upload.array('Image', 20), async (req, res, next) => {
     await find_and_set_dates();
 
     await dataupload(req, res);
-    return res.send("Succesfully uploaded")
+    return res.send("Successfully uploaded")
 
     // update the MongoDB Date depending on what you got a date through the user
     async function find_and_set_dates() {
